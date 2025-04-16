e3e17179636aa63fa9fff544ae38726f42049528
Fabian Viöl <fv@enqt.de>
2025-04-16 16:35:44 +0200

    Add support for changing interface type

--- src/iface/handle.rs
+++ src/iface/handle.rs
@@ -1,8 +1,29 @@
 // SPDX-License-Identifier: MIT
 
-use crate::{Nl80211Handle, Nl80211InterfaceGetRequest};
+use crate::{
+    Nl80211Attr, Nl80211AttrsBuilder, Nl80211Handle, Nl80211InterfaceGetRequest,
+};
+
+use super::{Nl80211InterfaceSetRequest, Nl80211InterfaceType};
 
 pub struct Nl80211InterfaceHandle(Nl80211Handle);
+
+#[derive(Debug)]
+pub struct Nl80211Interface;
+
+impl Nl80211Interface {
+    /// TODO: ....
+    pub fn new(if_index: u32) -> Nl80211AttrsBuilder<Self> {
+        Nl80211AttrsBuilder::<Self>::new().if_index(if_index)
+    }
+}
+
+impl Nl80211AttrsBuilder<Nl80211Interface> {
+    // TODO: better name? (type is a keyword)
+    pub fn interface_type(self, r#type: Nl80211InterfaceType) -> Self {
+        self.replace(Nl80211Attr::IfType(r#type))
+    }
+}
 
 impl Nl80211InterfaceHandle {
     pub fn new(handle: Nl80211Handle) -> Self {
@@ -13,5 +34,16 @@ impl Nl80211InterfaceHandle {
     /// (equivalent to `iw dev`)
     pub fn get(&mut self) -> Nl80211InterfaceGetRequest {
         Nl80211InterfaceGetRequest::new(self.0.clone())
+    }
+
+    /// Retrieve the wireless interfaces
+    /// (equivalent to `iw dev`)
+    // TODO: naming (set_...?)
+    // TODO: no restrictions on the type of attributes?
+    pub fn interface_type(
+        &mut self,
+        attributes: Vec<Nl80211Attr>,
+    ) -> Nl80211InterfaceSetRequest {
+        Nl80211InterfaceSetRequest::new(self.0.clone(), attributes)
     }
 }
--- src/iface/mod.rs
+++ src/iface/mod.rs
@@ -2,6 +2,7 @@
 
 mod combination;
 mod get;
+mod set;
 mod handle;
 mod iface_type;
 
@@ -10,7 +11,9 @@ pub use self::combination::{
     Nl80211IfaceCombLimitAttribute,
 };
 pub use self::get::Nl80211InterfaceGetRequest;
+pub use self::set::Nl80211InterfaceSetRequest;
 pub use self::handle::Nl80211InterfaceHandle;
 pub use self::iface_type::Nl80211InterfaceType;
+pub use self::handle::Nl80211Interface;
 
 pub(crate) use self::iface_type::Nl80211InterfaceTypes;
